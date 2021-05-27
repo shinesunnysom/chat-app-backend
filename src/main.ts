ca967319a38a7b81a10de586b2d78a092a8b0d94
Shinesunny Som <shinesunnysom@gmail.com>
2021-05-27 00:27:11 -0700

    gateway setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ChatModule } from './chat/chat.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
